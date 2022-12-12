f57f09e6b087dde4ca1b5defc0240648ec6e6ff3
deemtrglc <demet.merve00@outlook.com>
2022-12-12 20:54:50 +0300

    edit

--- lib/call_page.dart
+++ lib/call_page.dart
@@ -1,7 +1,9 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:zego_uikit_prebuilt_call/zego_uikit_prebuilt_call.dart';
+
+String userID = Random().nextInt(10000).toString();
 
 class CallPage extends StatelessWidget {
   const CallPage({Key? key, required this.callID}) : super(key: key);
@@ -11,11 +13,11 @@ class CallPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return ZegoUIKitPrebuiltCall(
       appID:
-          yourAppID, // **Fill in the appID that you get from ZEGOCLOUD Admin Console.
+           yourAppID, // **Fill in the appID that you get from ZEGOCLOUD Admin Console.
       appSign:
           "yourAppSign", // **Fill in the appSign that you get from ZEGOCLOUD Admin Console.
-      userID: 'user_id',
-      userName: 'user_name',
+      userID: userID,
+      userName: 'user_$userID',
       callID: callID,
       // You can also use groupVideo/groupVoice/oneOnOneVoice to make more types of calls.
       config: ZegoUIKitPrebuiltCallConfig.oneOnOneVideoCall()
